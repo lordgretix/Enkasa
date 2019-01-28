77cb6f055c3b11819e71458c6e5d09edeea3758b
lordgretix <mrmaster@hotmail.es>
2019-01-28 13:28:00 +0100

    trash

--- app/src/main/java/com/gp3/enkasa/LoginActivity.java
+++ app/src/main/java/com/gp3/enkasa/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -65,52 +66,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
         progressDialog.show();
-        new Handler().post(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        });
-
-        try {
-            URL url = new URL("https://kasserver.synology.me/etazi/get_json.php");
-            String params = "db=reto_gp3&users_table=usuarios&username_field=nombre&password_field=password&username="+username+"&password="+password+"&data_table[]=alojamientos&get_user=true&hash=sha256";
-            byte[] postDataBytes = params.getBytes("UTF-8");
-
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.getOutputStream().write(postDataBytes);
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-            StringBuilder response= new StringBuilder();
-            String buff;
-            while((buff = in.readLine())!=null){
-                response.append(buff);
-            }
-
-            Data data =  new Gson().fromJson(response.toString(), Data.class);
-
-            System.out.println("Data has error: "+data.hashError());
-            if(data.hashError()){
-                Toast.makeText(getApplicationContext(), R.string.login_status_failed, Toast.LENGTH_SHORT).show();
-                mTxtUserName.setError(getResources().getString(R.string.login_status_failed));
-            }else{
-                Toast.makeText(getApplicationContext(), R.string.login_status_logged, Toast.LENGTH_SHORT).show();
-                mTxtUserName.setError(getResources().getString(R.string.login_status_logged));
-
-                createAlojamientos(data);
-            }
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        } finally {
-            progressDialog.dismiss();
-        }
-
-        /*new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -151,7 +107,7 @@ public class LoginActivity extends AppCompatActivity {
                     progressDialog.dismiss();
                 }
             }
-        }, 2000);*/
+        }, 2000);
 
     }
 
